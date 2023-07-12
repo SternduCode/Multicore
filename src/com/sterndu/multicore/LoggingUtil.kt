00f7845ecfaa50ad799a3d5cb2ea7a4a27bc1abd
Sterndu <Sterndu13570@gmail.com>
2023-07-13 00:20:50 +0200

    Fixed Java 8 Compatibility

--- src/com/sterndu/multicore/LoggingUtil.kt
+++ src/com/sterndu/multicore/LoggingUtil.kt
@@ -3,10 +3,10 @@ package com.sterndu.multicore
 
 import java.io.File
 import java.io.IOException
+import java.time.Instant
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.util.logging.*
-import kotlin.jvm.Throws
 
 object LoggingUtil {
 
@@ -21,10 +21,10 @@ object LoggingUtil {
 		initialized = true
 
 		consoleHandler = ConsoleHandler()
-		consoleHandler.formatter = CustomFormatter()
+		consoleHandler.formatter = CustomFormatterConsole()
 		fileHandler = FileHandler(String.format("logs/log-%td.%1\$tm.%1\$tY-%%u.log", ZonedDateTime.now()), true)
 		fileHandler.level = Level.ALL
-		fileHandler.formatter = CustomFormatter()
+		fileHandler.formatter = CustomFormatterFile()
 
 		var day = System.currentTimeMillis() - System.currentTimeMillis() % secondsOfADay
 
@@ -34,7 +34,7 @@ object LoggingUtil {
 					day = System.currentTimeMillis() - System.currentTimeMillis() % secondsOfADay
 					val newFileHandler = FileHandler(String.format("logs/log-%td.%tm.%tY.log", ZonedDateTime.now()), true)
 					newFileHandler.level = Level.ALL
-					newFileHandler.formatter = CustomFormatter()
+					newFileHandler.formatter = CustomFormatterFile()
 					val logManager = LogManager.getLogManager()
 					for (name in logManager.loggerNames) {
 						val logger = logManager.getLogger(name)
@@ -81,11 +81,21 @@ object LoggingUtil {
 		}
 	}
 
-	class CustomFormatter: Formatter() {
+	class CustomFormatterFile: Formatter() {
 
 		override fun format(record: LogRecord): String {
 			return String.format("[%1\$td.%1\$tm.%1\$tY %1\$tH:%1\$tM:%1\$tS.%1\$tL%1\$tz][%1\$tQ][%3\$s]: %4\$s: %5\$s %6\$s%n",
-				record.instant.atZone(ZoneId.systemDefault()), record.sourceClassName + "." + record.sourceMethodName, record.loggerName,
+				Instant.ofEpochMilli(record.millis).atZone(ZoneId.systemDefault()), record.sourceClassName + "." + record.sourceMethodName, record.loggerName,
+				record.level.name, record.message, record.thrown?.toString() ?: "")
+		}
+
+	}
+
+	class CustomFormatterConsole: Formatter() {
+
+		override fun format(record: LogRecord): String {
+			return String.format("[%1\$td.%1\$tm.%1\$tY %1\$tH:%1\$tM:%1\$tS.%1\$tL%1\$tz][%3\$s]: %4\$s: %5\$s %6\$s%n",
+				Instant.ofEpochMilli(record.millis).atZone(ZoneId.systemDefault()), record.sourceClassName + "." + record.sourceMethodName, record.loggerName,
 				record.level.name, record.message, record.thrown?.toString() ?: "")
 		}
 
